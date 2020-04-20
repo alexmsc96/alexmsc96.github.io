879521c09d78223925c277186f4b15b752ef4578
alexmsc96 <62430558+alexmsc96@users.noreply.github.com>
2020-04-20 22:44:41 +0300

    Update script.js

--- weather-app/script.js
+++ weather-app/script.js
@@ -17,7 +17,8 @@ function getWeather(event){
         return;
     }
     let city = cityInput.value;
-    const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5fd991c02604126a3729cb3a26e017ed&units=metric`
+    proxy = 'https://cors-anywhere.herokuapp.com/';
+    const api = `${proxy}://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5fd991c02604126a3729cb3a26e017ed&units=metric`
     fetch(api)
         .then(response => {
             return response.json()
