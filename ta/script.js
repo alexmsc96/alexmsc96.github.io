c640c353e5cfdb038f4b13a921f72a982f6ce98e
alexmsc96 <alexandrumuscina@gmail.com>
2021-04-15 00:09:19 +0300

    Commit

--- ta/script.js
+++ ta/script.js
@@ -1,14 +1,1 @@
-let mySwiper = new Swiper(".swiper-container", {
-  direction: "horizontal",
-  loop: true,
-  grabCursor: true,
-  pagination: {
-    el: ".swiper-pagination",
-    dynamicBullets: true,
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+var swiper = new Swiper('.swiper-container');
